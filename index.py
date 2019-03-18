4c50f7405b97b2b6127849589510b18f5c09bbc0
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-03-17 21:54:08 -0400

    Add changes to python code

--- index.py
+++ index.py
@@ -3,27 +3,29 @@ file = open(f,'r')
 
 print file.read()
 
-
-d1a = input ("What do you want to do?:\n\
+options = input ("What do you want to do?:\n\
     1)Sort alphabetically by first name\n\
     2)Sort alphabetically by last name\n\
-    3)Sort by total length\n\
+    3)Sort by the total length\n\
     4)Reverse the list\n\
     5)Correctly capitalize the list\n\
     [1/2/3/4/5]: ")
-if d1a == "1":
-    lines = file.readlines()
-    lines.sort()
-elif d1a == "2":
+if options == "1":
+    for line in f:
+        line = line.sort()
+        print(f)
+elif options == "2":
     print ("You approach the stables.")
-elif d1a == "3":
+elif options == "3":
     print ("You approach the stables.")
-elif d1a == "4":
+elif options == "4":
     print ("You approach the stables.")
-elif d1a == "5":
+elif options == "5":
     print ("You approach the stables.")
 
-        
 
 file.close()
+
+ 
+
 
