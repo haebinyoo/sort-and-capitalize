01abf3a5b4234f26ffa1802b2de194f2f01082f1
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-03-23 19:22:05 -0400

    Capitalize names in the list

--- index.py
+++ index.py
@@ -2,19 +2,24 @@ f = raw_input('Which file do you want?')
 file = open(f,'r')
 list1 = []
 list1 = file.readlines()
-print list1
 list1 = [x.strip() for x in list1]
+list1 = [x.title() for x in list1]
 print list1
-##file = open(f,'w')
-##print("1. Sort alphabetically by first name")
-##print("2. Sort alphabetically by last name")
-##inp = int(input("What would you like to do?"))
-##if inp==1:
-##        print file.sort()
-##        
-##elif inp==2:
-##        inp = file.write("bob")
-##else:
-##        print file.write("Invalid")
-##               
-##file.close()
+file = open(f,'w')
+print("1. Sort alphabetically by first name")
+print("2. Sort alphabetically by last name")
+print("5. Capitalize the list of name")
+
+inp = int(input("What would you like to do?"))
+if inp==1:
+        list1.sort()
+        print list1
+        
+elif inp==2:
+        inp = file.write("bob")
+elif inp==5:
+        [file.write(x, '\n') for x in list1]
+else:
+        print file.write("Invalid")
+               
+file.close()
