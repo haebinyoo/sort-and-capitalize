1d652d386a5bce43b792796dfaa6b5b504191647
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-03-23 22:30:54 -0400

    Sort the list alphabetically by last name and write the change in the file

--- index.py
+++ index.py
@@ -8,20 +8,26 @@ print list1
 file = open(f,'w')
 print("1. Sort alphabetically by first name")
 print("2. Sort alphabetically by last name")
-print("5. Capitalize the list of name")
+print("3. Sort by the total lenght")
+print("4. Reverse the list")
+print("5. Capitalize the list")
 
 inp = int(input("What would you like to do?"))
 if inp==1:
         list1.sort()
         print list1
         [file.write(x + '\n') for x in list1]
-        
 elif inp==2:
-        inp = file.write("bob")
-elif inp==5:
+        list1.sort(key=lambda x: x.split(" ")[-1].lower()) 
         [file.write(x + '\n') for x in list1]
+##elif inp==3:
+##        [file.write(x + '\n') for x in list1]
+##elif inp==4:
+##        [file.write(x + '\n') for x in list1]
+##elif inp==5:
+##        [file.write(x + '\n') for x in list1]
         
 else:
-        print file.write("Invalid")
+        print file.write("Invalid Request Enter 1-5")
                
 file.close()
